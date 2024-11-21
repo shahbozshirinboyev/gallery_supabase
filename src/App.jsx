ba4dcdb52e48e6846bed4738cfef62d7838e51cc
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-11-21 16:52:23 +0500

    Adding new function All img info to sava

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { supabase } from "./services/supabase";
 
 function App() {
-
   const [gallery, setGallery] = useState([]);
 
   const getImgData = async () => {
@@ -13,30 +12,33 @@ function App() {
       const { data, error } = await supabase.storage
         .from(bucketName)
         .list(bucketFolderName, { limit: 100, offset: 0 });
-
-        console.log(data)
-
       if (error) {
         throw error;
       }
+
       const imageUrls = await Promise.all(
-        data.map(async (file) => {
+        data.map(async (fileInfo) => {
           const { data: publicData, error } = supabase.storage
             .from(bucketName)
-            .getPublicUrl(file.name);
+            .getPublicUrl(fileInfo.name);
 
           if (error) {
-            console.error(error.message);
+            console.error(
+              `Error fetching public URL for ${fileInfo.name}:`,
+              error.message
+            );
             return null;
           }
-          return publicData.publicUrl;
+          return {
+            ...fileInfo,
+            publicUrl: publicData.publicUrl,
+          };
         })
       );
 
-      // Faqat muvaffaqiyatli URL larni saqlash
-      setGallery(imageUrls.filter((url) => url !== null));
+      setGallery(imageUrls.filter((item) => item !== null));
     } catch (error) {
-      console.error(error.message);
+      console.error("Error fetching images:", error.message);
     }
   };
 
@@ -51,17 +53,24 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-4 gap-4">
-        {gallery.map((url, index) => (
+        {gallery.map((image, index) => (
           <div
             key={index}
-            className="relative group w-full h-[200px] border overflow-hidden"
+            className="relative group w-full overflow-hidden p-2 border rounded-md"
           >
             {/* Tasvir */}
             <img
-              src={url}
+              src={image.publicUrl}
               alt={`Image ${index}`}
-              className="w-full h-full object-cover"
+              className="w-full h-[200px] object-cover rounded-md"
             />
+            <div className="pt-2">
+              <span>Name: </span> <span>{image.name}</span>
+              <br />
+              <span>Size: </span>{" "}
+              <span>{Math.round(image.metadata.size / 1024).toFixed(1)}</span>{" "}
+              <span>Kb</span>
+            </div>
             <div className="absolute inset-0 bg-red-700 bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
           </div>
         ))}
